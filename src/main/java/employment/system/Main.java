a3d6a919df6865ed91ef606d974a3da9fe870c46
DragosAdrian <adrian123795@gmail.com>
2021-04-05 14:48:51 +0300

    ESFI-13 - Refactoring Main file to open the application once it is run.

---
+++ src/main/java/employment/system/Main.java
@@ -0,0 +1,27 @@
+package employment.system;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
+        primaryStage.setTitle("Registration Example");
+        primaryStage.setScene(new Scene(root, 500, 300));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
